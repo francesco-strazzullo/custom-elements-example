019148f87f0cdf763792a67ff5a64bc739a63841
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2018-02-17 22:53:28 +0100

    modifing disabled attribute

--- app/components/App/App.js
+++ app/components/App/App.js
@@ -4,6 +4,24 @@ import htmlToDomElement from '../../utils/htmlToDomElement'
 class App extends HTMLElement {
   connectedCallback () {
     this.appendChild(htmlToDomElement(template))
+
+    this
+      .querySelector('button[role="toggle"]')
+      .addEventListener('click', () => {
+        const firstLabel = document.getElementById('first')
+        const secondLabel = document.getElementById('second')
+        const thirdLabel = document.getElementById('third')
+
+        firstLabel.disabled = !firstLabel.disabled
+
+        if (secondLabel.hasAttribute('disabled')) {
+          secondLabel.removeAttribute('disabled')
+        } else {
+          secondLabel.setAttribute('disabled', '')
+        }
+
+        thirdLabel.toggle()
+      })
   }
 }
 
--- app/components/CustomLabel/CustomLabel.js
+++ app/components/CustomLabel/CustomLabel.js
@@ -1,7 +1,23 @@
 class CustomLabel extends HTMLElement {
   connectedCallback () {
-    const disabled = this.hasAttribute('disabled') ? 'Yes' : 'No'
+    const disabled = this.disabled ? 'Yes' : 'No'
     this.innerHTML = `<span class="text-gray">Am I disabled? ${disabled}</span>`
+  }
+
+  get disabled () {
+    return this.hasAttribute('disabled')
+  }
+
+  set disabled (newDisabled) {
+    if (newDisabled) {
+      this.setAttribute('disabled', '')
+    } else {
+      this.removeAttribute('disabled')
+    }
+  }
+
+  toggle () {
+    this.disabled = !this.disabled
   }
 }
 
